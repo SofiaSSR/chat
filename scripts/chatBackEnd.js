d08a48c00a999c0c99a9bf92b9d6451b6df58831
SofiaSSR <unitargetofsofi@gmail.com>
2020-02-19 16:57:53 -0500

    base

--- scripts/chatBackEnd.js
+++ scripts/chatBackEnd.js
@@ -1,21 +1,9 @@
-var Pre = "type a message",txt = "";
-function write(n){
-     if (txt == ""){
-        document.getElementById("textM").innerHTML = Pre;
-     }else if (txt.length<=2000){
-    txt = txt + n;
-    document.getElementById("textM").innerHTML = txt;
-      
-    }
-      };
-document.addEventListener('keyup', (e) => {
-    
-    if (48 <= e.keyCode <= 90 ){
-        console.log(e.key+ "  "+e.keyCode);
-        write(e.key);
-    if (e.key == "Backspace"){
-        if(txt.length > 1){
-        txt = txt.substring(0, txt.length - 1);
-        document.getElementById("textM").innerHTML = txt;}
+var txt = "";
+function sent(n){
+    document.getElementById("textM").value = txt
+     if (txt != ""){
          
-        }}});
+        
+     }
+      
+      };
